15de9e68269c83d4e3c2454dcffee260fd4f3c1e
Lucas Furlani <lrosa@upei.ca>
2013-11-20 00:14:31 -0400

    Updated class Road.

--- week11/W_Road.cpp
+++ week11/W_Road.cpp
@@ -7,3 +7,166 @@
 //
 
 #include "W_Road.h"
+#include <stdio.h>
+namespace wolf {
+    
+    struct Vertex
+    {
+        GLfloat x,y,z;
+        GLfloat u,v;
+    };
+    
+    static const Vertex cubeVertices[] = {
+        /*
+         // Front
+         { -0.5f, -0.5f, 0.5f, 0, 0 },
+         { -0.5f,  0.5f, 0.5f, 0, 1 },
+         {  0.5f,  0.5f, 0.5f, 1, 1 },
+         {  0.5f,  0.5f, 0.5f, 1, 1 },
+         {  0.5f, -0.5f, 0.5f, 1, 0 },
+         { -0.5f, -0.5f, 0.5f, 0, 0 },
+         
+         // Back
+         {  0.5f,  0.5f,-0.5f, 1, 1 },
+         { -0.5f,  0.5f,-0.5f, 0, 1 },
+         { -0.5f, -0.5f,-0.5f, 0, 0 },
+         { -0.5f, -0.5f,-0.5f, 0, 0 },
+         {  0.5f, -0.5f,-0.5f, 1, 0 },
+         {  0.5f,  0.5f,-0.5f, 1, 1 },
+         
+         // Left
+         { -0.5f,  0.5f,-0.5f, 0, 1 },
+         { -0.5f,  0.5f, 0.5f, 1, 1 },
+         { -0.5f, -0.5f, 0.5f, 1, 0 },
+         { -0.5f, -0.5f, 0.5f, 1, 0 },
+         { -0.5f, -0.5f,-0.5f, 0, 0 },
+         { -0.5f,  0.5f,-0.5f, 0, 1 },
+         
+         // Right
+         {  0.5f,  0.5f, 0.5f, 0, 1 },
+         {  0.5f,  0.5f,-0.5f, 1, 1 },
+         {  0.5f, -0.5f,-0.5f, 1, 0 },
+         {  0.5f, -0.5f,-0.5f, 1, 0 },
+         {  0.5f, -0.5f, 0.5f, 0, 0 },
+         {  0.5f,  0.5f, 0.5f, 0, 1 },
+         
+         // Top
+         { -0.5f,  0.5f, 0.5f, 0, 0 },
+         { -0.5f,  0.5f,-0.5f, 0, 1 },
+         {  0.5f,  0.5f,-0.5f, 1, 1 },
+         {  0.5f,  0.5f,-0.5f, 1, 1 },
+         {  0.5f,  0.5f, 0.5f, 1, 0 },
+         { -0.5f,  0.5f, 0.5f, 0, 0 },
+         */
+        // Bottom
+        { -0.5f, -0.5f, 0.5f, 0, 0 },
+        {  0.5f, -0.5f, 0.5f, 1, 0 },
+        {  0.5f, -0.5f,-0.5f, 1, 1 },
+        {  0.5f, -0.5f,-0.5f, 1, 1 },
+        { -0.5f, -0.5f,-0.5f, 0, 1 },
+        { -0.5f, -0.5f, 0.5f, 0, 0 },
+    };
+    //static GLuint tex;
+    static wolf::Texture* wallTexture[8];
+    //----------------------------------------------------------
+    // Constructor
+    //----------------------------------------------------------
+    //              qpVB                qpDecl              q_pProgram
+    Road::Road() : vertexBuffer(0), vertexDeclaration(0), scaleMatrix(0), projectionMatrix(0), viewMatrix(0)
+    {
+        
+        
+        vertexBuffer = wolf::BufferManager::CreateVertexBuffer(cubeVertices, sizeof(Vertex) * 1 * 3 * 2);
+        
+        vertexDeclaration = new wolf::VertexDeclaration();
+        vertexDeclaration->Begin();
+        vertexDeclaration->AppendAttribute(wolf::AT_Position, 3, wolf::CT_Float);
+        //vertexDeclaration->AppendAttribute(wolf::AT_Color, 4, wolf::CT_UByte);
+        vertexDeclaration->AppendAttribute(wolf::AT_TexCoord1, 2, wolf::CT_Float);
+        vertexDeclaration->SetVertexBuffer(vertexBuffer);
+        vertexDeclaration->End();
+        
+        /*
+         // Handle texture
+         glGenTextures(1, &tex);
+         glBindTexture(GL_TEXTURE_2D, tex);
+         
+         GLFWimage img;
+         glfwReadImage( "data/week11/building1.tga", &img, 0 );
+         glTexImage2D(GL_TEXTURE_2D, 0, img.Format, img.Width, img.Height, 0, img.Format, GL_UNSIGNED_BYTE, img.Data);
+         glfwFreeImage(&img);
+         
+         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+         */
+        //wolf::Texture* pTex = wolf::TextureManager::CreateTexture("data/week11/building1.tga");
+        // TODO: MAKE AN ARRAY
+        for (int i = 0; i < 2; i++) {
+            //std::string s = string(itoa(i));
+            //std::string textureName = std::string("data/week11/building/") + std::string(texture);
+            char buffer [50];
+            char char_texture = i + '0';
+            sprintf (buffer, "data/week11/textures/road/%c.tga", char_texture);
+            
+            //wallTexture[i] = wolf::TextureManager::CreateTexture(sprintf("data/week11/building/%hd.tga", texture));
+            wallTexture[i] = wolf::TextureManager::CreateTexture(buffer);
+        }
+        
+        
+        //g_pTexture->SetWrapMode(wolf::Texture::WM_Repeat, wolf::Texture::WM_Repeat);
+        
+        // Initialize matrices values
+        translationMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+        scaleMatrix       = glm::scale(1.0f, 1.0f, 1.0f);
+        rotationMatrix    = glm::rotate(0.0f, 1.0f, 0.0f, 0.0f);
+    }
+    
+    void Road::SetPosition(const glm::vec3& position) {
+        translationMatrix = glm::translate(glm::mat4(1.0f), position);;
+    }
+    
+    glm::mat4 Road::GetPosition() {
+        return translationMatrix;
+    }
+    
+    void Road::SetScale(float x, float y, float z) {
+        scaleMatrix = glm::scale(x, y, z);
+    }
+    
+    void Road::SetRotation(float angle, float x, float y, float z) {
+        rotationMatrix = glm::rotate(angle, x, y, z);
+    }
+    
+    void Road::SetTexture(short _texture) {
+        texture = _texture;
+    }
+    
+    void Road::SetType(short _type) {
+        type = _type;
+    }
+    
+    void Road::Render(wolf::Program* program, glm::mat4* _projectionMatrix, glm::mat4* _viewMatrix, glm::mat4 _worldMatrix)
+    {
+        projectionMatrix = _projectionMatrix;
+        viewMatrix = _viewMatrix;
+        worldMatrix = translationMatrix * scaleMatrix * rotationMatrix;
+        
+        
+        // Use shader program.
+        program->Bind();
+        
+        // Bind texture
+        wallTexture[texture]->Bind();
+        
+        // Bind Uniforms
+        program->SetUniform("projection", *projectionMatrix);
+        program->SetUniform("view", *viewMatrix);
+        program->SetUniform("world", worldMatrix);
+        program->SetUniform("texture1", 0);
+        // Set up source data
+        vertexDeclaration->Bind();
+        
+        // Draw!
+        glDrawArrays(GL_TRIANGLES, 0, 1 * 3 * 2);
+    }
+}
--- week11/W_Road.h
+++ week11/W_Road.h
@@ -10,5 +10,71 @@
 #define __week11__W_Road__
 
 #include <iostream>
+#include "W_VertexBuffer.h"
+#include "W_IndexBuffer.h"
+#include "W_Program.h"
+#include "W_Common.h"
+#include "W_BufferManager.h"
+#include "W_VertexDeclaration.h"
+#include "W_ProgramManager.h"
+#include "W_TextureManager.h"
+
+#include <string>
+#include <map>
+#include <stdio.h>
+
+
+namespace wolf
+{
+    class Road
+    {
+	public:
+		//-------------------------------------------------------------------------
+		// PUBLIC INTERFACE
+		//-------------------------------------------------------------------------
+        //Road(glm::mat4* _projectionMatrix, glm::mat4* _viewMatrix);
+        Road();
+        // Draw the ceiling in the screen
+        void Render(wolf::Program* program, glm::mat4* _projectionMatrix, glm::mat4* _viewMatrix, glm::mat4 worldMatrix);
+		void SetPosition(const glm::vec3& position);
+        glm::mat4 GetPosition();
+        
+        void SetScale(float x, float y, float z);
+        void SetRotation(float angle, float x, float y, float z);
+        void SetTexture(short _texture);
+        void SetType(short _type);
+        
+		//-------------------------------------------------------------------------
+        
+	private:
+		//-------------------------------------------------------------------------
+		// PRIVATE TYPES
+		//-------------------------------------------------------------------------
+		//-------------------------------------------------------------------------
+        
+		//-------------------------------------------------------------------------
+		// PRIVATE MEMBERS
+		//-------------------------------------------------------------------------
+		wolf::VertexBuffer*			  vertexBuffer; // qpVB
+		wolf::VertexDeclaration* vertexDeclaration; //qpDecl
+        
+        // Uniforms
+        glm::mat4* projectionMatrix;
+        glm::mat4* viewMatrix;
+        glm::mat4 worldMatrix;
+        
+        // World Matrix components
+        glm::mat4 translationMatrix;
+        glm::mat4 scaleMatrix;
+        glm::mat4 rotationMatrix;
+        
+        // Road parameters
+        short texture;
+        short type;
+		//-------------------------------------------------------------------------
+        
+    };
+    
+}
 
 #endif /* defined(__week11__W_Road__) */
